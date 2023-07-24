8dee6f190f8a309a675460c82dc03aecca1925b1
Andrew Harnick <56137506+har-nick@users.noreply.github.com>
2023-07-24 16:00:19 +0100

    Amended Ktor config

--- src/commonMain/kotlin/uk/co/harnick/bandkit/di/SharedModule.kt
+++ src/commonMain/kotlin/uk/co/harnick/bandkit/di/SharedModule.kt
@@ -1,8 +1,24 @@
 package uk.co.harnick.bandkit.di
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel.INFO
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val sharedModule = module {
-    single { HttpClient() }
+    single {
+        HttpClient {
+            install(Logging) {
+                level = INFO
+            }
+            install(ContentNegotiation) {
+                json(Json)
+            }
+
+            expectSuccess = true
+        }
+    }
 }
