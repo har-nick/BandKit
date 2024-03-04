8cb6277458359032a77719af877ecaa3be8ac404
Andrew Harnick <56137506+har-nick@users.noreply.github.com>
2024-03-04 02:34:13 +0000

    Updated to fix Windows + CIO engine compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,56 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlinx.serialization)
+    id("maven-publish")
 }
 
 group = "uk.co.harnick"
 version = "1.0-SNAPSHOT"
 
+android {
+    compileSdk = 34
+    namespace = "some.namespace.previously.in.android.manifest"
+    defaultConfig {
+        minSdk = 24
+        compileSdk = 34
+    }
+}
+
 kotlin {
-    explicitApi()
     jvmToolchain(libs.versions.jdk.get().toInt())
-}
+    withSourcesJar(publish = false)
+    explicitApi()
 
-dependencies {
-    implementation(libs.bundles.ktor)
-    implementation(libs.kotlinx.datetime)
+    applyDefaultHierarchyTemplate()
+    js {
+        browser()
+        nodejs()
+    }
+    jvm()
+    androidTarget()
+    iosArm64()
+    linuxArm64()
+    mingwX64("windows")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
+    sourceSets {
+        val windowsMain by getting
 
-tasks.test {
-    useJUnitPlatform()
+        commonMain.dependencies {
+            implementation(libs.bundles.ktor)
+            implementation(libs.kotlinx.datetime)
+        }
+
+        listOf(jvmMain, androidMain, iosMain, linuxMain).forEach {
+            it.dependencies {
+                implementation(libs.ktor.engine.cio)
+            }
+        }
+        jsMain.dependencies {
+            implementation(libs.ktor.engine.js)
+        }
+        windowsMain.dependencies {
+            implementation(libs.ktor.engine.winhttp)
+        }
+    }
 }
