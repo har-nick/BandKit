237f6e9f91cd00910af972e0f6c99144a8c87c2a
Andrew Harnick <56137506+har-nick@users.noreply.github.com>
2024-06-27 01:07:05 +0100

    Improved token validation

--- src/commonMain/kotlin/uk/co/harnick/bandkit/data/model/BandcampToken.kt
+++ src/commonMain/kotlin/uk/co/harnick/bandkit/data/model/BandcampToken.kt
@@ -1,7 +1,8 @@
 package uk.co.harnick.bandkit.data.model
 
-import kotlin.jvm.JvmInline
+import kotlinx.serialization.Serializable
 import uk.co.harnick.bandkit.data.remote.api.BandKit
+import kotlin.jvm.JvmInline
 
 // TODO: VALIDATION
 /**
@@ -9,5 +10,15 @@ import uk.co.harnick.bandkit.data.remote.api.BandKit
  * @param [token] You should
  * @exception [IllegalArgumentException] If parameter [token] does not pass validation.
  */
+@Serializable
 @JvmInline
-public value class BandcampToken(public val token: String)
+public value class BandcampToken(public val token: String) {
+    private fun tokenIsUrlEncoded(token: String): Boolean {
+        val regex = "^[A-Za-z0-9%+/=]+$".toRegex()
+        return token.matches(regex)
+    }
+
+    init {
+        require(tokenIsUrlEncoded(token)) { "Token is not URL-Encoded" }
+    }
+}
