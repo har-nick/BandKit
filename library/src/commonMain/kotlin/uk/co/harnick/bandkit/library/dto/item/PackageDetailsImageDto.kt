74f72fff8dbdc7bfb047810f737d8899c017abcb
har-nick <56137506+har-nick@users.noreply.github.com>
2024-12-01 22:41:02 +0000

    Fixed class visibility

--- library/src/commonMain/kotlin/uk/co/harnick/bandkit/library/dto/item/PackageDetailsImageDto.kt
+++ library/src/commonMain/kotlin/uk/co/harnick/bandkit/library/dto/item/PackageDetailsImageDto.kt
@@ -4,13 +4,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal class PackageDetailsImageDto private constructor(
+public class PackageDetailsImageDto private constructor(
     @SerialName("image_id")
-    val id: Long,
+    public val id: Long,
 
     @SerialName("width")
-    val width: Int,
+    public val width: Int,
 
     @SerialName("height")
-    val height: Int
+    public val height: Int
 )
